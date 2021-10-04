21eac2869e4894cf514e3382fa1f8a7259b72e6b
Anita Readman <anita.readman@sita.aero>
2021-10-04 09:56:44 +0100

    Complete all exercises in JavaBuzz chapter - arrays and control flow

--- chapter3-JavaBuzz/exercises/JavaBuzz.java
+++ chapter3-JavaBuzz/exercises/JavaBuzz.java
@@ -1,5 +1,20 @@
 // can you remember how to set up your main function?
 
+class JavaBuzz {
+    public static void main(String args[]) {
+        for(int i=1; i<101; i++) {
+            if(i % 15 == 0) {
+                System.out.print("JavaBuzz");
+            } else if(i % 5 == 0) {
+                System.out.print("Buzz");
+            } else if(i % 3 == 0) {
+                System.out.print("Java");
+            } else {
+                System.out.print(i);
+            }
+        }
+    }
+}
 // "Java" if divisible by 3
 // "Buzz" if divisible by 5
 // "JavaBuzz" if divisible by 15
--- chapter3-JavaBuzz/exercises/LoopThroughArray.java
+++ chapter3-JavaBuzz/exercises/LoopThroughArray.java
@@ -1,3 +1,9 @@
 // can you remember how to set up your main function?
-
+class LoopThroughArray {
+    public static void main(String args[]) {
+        for(int i=1; i<=5; i++) {
+            System.out.println(i);
+        }
+    }
+}
 // loop through an integer array and print out each element
